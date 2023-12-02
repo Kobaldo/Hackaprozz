80754f73a88b96014e6a83958af32fe5806c9868
Rodrigo Martins <84147797+Kobaldo@users.noreply.github.com>
2023-12-02 19:52:13 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const botaoGerar = document.getElementById("botaoGerar");
+    if (botaoGerar) {
+        botaoGerar.addEventListener("click", gerarPersonagem);
+    }
+});
+
+function gerarPersonagem() {
+    // DICA: Obter valores dos elementos de entrada (raca, classe, genero)
+    
+    // DICA: Chamar função para gerar atributos
+
+    // DICA: Chamar função para gerar nome do personagem
+
+    // DICA: Chamar função para exibir personagem
+}
+
+function gerarAtributos(raca, classe) {
+    // DICA: Inicializar atributosBase
+
+    // DICA: Chamar função para obter modificadores de raça
+
+    // DICA: Chamar função para obter modificadores de classe
+
+    // DICA: Aplicar modificadores de raça e classe
+
+    // DICA: Adicionar variação aleatória
+
+    // DICA: Retornar atributosBase
+}
+
+function obterModificadoresRaca(raca) {
+    // DICA: Definir modificadores de raça
+
+    // DICA: Retornar modificadores correspondentes à raça
+}
+
+function obterModificadoresClasse(classe) {
+    // DICA: Definir modificadores de classe
+
+    // DICA: Retornar modificadores correspondentes à classe
+}
+
+function aplicarModificadores(atributos, modificadores) {
+    // DICA: Aplicar modificadores aos atributos
+}
+
+function gerarNomePersonagem(raca, genero) {
+    // DICA: Definir nomes de acordo com raça e genero
+
+    // DICA: Escolher nome e sobrenome aleatórios
+
+    // DICA: Retornar nome completo
+}
+
+function exibirPersonagem(nome, atributos) {
+    // DICA: Exibir nome e atributos na ficha do personagem
+}
